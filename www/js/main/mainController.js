a90ba23b8356e9b7d59e356061b39e0792df2f18
Valdeci Gomes <valdeci@coderade.in>
2016-08-07 16:29:27 -0300

    implement delete, favorite and pull to refresh

--- www/js/main/mainController.js
+++ www/js/main/mainController.js
@@ -3,10 +3,26 @@ ionicApp.controller("mainController", ['$scope', '$http',
     $http.get('data/data.json').success(
       function (data) {
         $scope.games = data;
+
         $scope.moveGame = function (game, fromIndex, toIndex) {
           $scope.games.splice(fromIndex, 1);
           $scope.games.splice(toIndex, 0, game);
+        };
 
+        $scope.onGameDelete=  function (game) {
+          $scope.games.splice($scope.games.indexOf(game), 1)
+        };
+
+        $scope.toggleStar = function (game) {
+          game.star = !game.star;
+        };
+
+        $scope.doRefresh = function () {
+          $http.get('data/data.json').success(
+            function (data) {
+              $scope.games = data;
+              $scope.$broadcast('scroll.refreshComplete');
+            });
         }
       }
     )
